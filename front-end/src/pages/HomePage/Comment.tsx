d5dfd64d73bf6c6500575fad27f31b4e70ce44a1
Daniel Valchev <dvalchevvv@gmail.com>
2024-07-02 16:14:22 +0300

    Fixes

--- front-end/src/pages/HomePage/Comment.tsx
+++ front-end/src/pages/HomePage/Comment.tsx
@@ -6,7 +6,7 @@ const CommentComponent = () => {
             <img src={profile_picture} className='h-6' alt="" />
             <span className="font-semibold text-sm content-center">Username</span>
         </div>
-        <div className="pt-2 text-sm font">
+        <div className="pt-1 text-sm font">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </div>
     </div>
