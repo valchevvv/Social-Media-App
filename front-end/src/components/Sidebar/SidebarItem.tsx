c147967b7b17d48bc8153f220a682fe5b02aa3cc
Daniel Valchev <dvalchevvv@gmail.com>
2024-07-10 16:18:36 +0300

    Fixes

--- front-end/src/components/Sidebar/SidebarItem.tsx
+++ front-end/src/components/Sidebar/SidebarItem.tsx
@@ -11,8 +11,9 @@ interface SidebarItemProps {
 
 const SidebarItem = ({ icon: Icon, iconOnly, label, to }: SidebarItemProps) => {
     const location = useLocation();
+    const isActive = location.pathname == to || (to == "/profile" && location.pathname.includes("profile"));
 
-    return <Link to={to} className={`${(iconOnly ? "text-white text-3xl justify-center" : "text-xl")} w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white ${(location.pathname != to ? "hover:bg-gray-700" : "bg-gray-700")} dark:hover:bg-gray-700 group`}>
+    return <Link to={to} className={`${(iconOnly ? "text-white text-3xl justify-center" : "text-xl")} w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white ${(isActive ? "bg-gray-700" : "hover:bg-gray-700")} dark:hover:bg-gray-700 group`}>
         <Icon />
         <span className={`${(iconOnly && "hidden")} ms-2`}>{label}</span>
     </Link >
