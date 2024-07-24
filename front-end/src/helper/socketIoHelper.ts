d67cbee5951f355abf095a50b48d93720a1dc533
Daniel Valchev <dvalchevvv@gmail.com>
2024-07-24 15:04:13 +0300

    Fixes

--- front-end/src/helper/socketIoHelper.ts
+++ front-end/src/helper/socketIoHelper.ts
@@ -1,4 +1,3 @@
-// socketIoHelper.ts
 import { io, Socket } from 'socket.io-client';
 import { jwtDecode } from 'jwt-decode';
 import { notifyInfo } from './notificationHelper';
@@ -12,134 +11,127 @@ interface DecodedToken {
 const debug = false;
 
 export class SocketIoHelper {
-    private socket: Socket | null = null;
-    private userId: string | null = null;
-    private token: string | null = null;
+    private socket: Socket;
+    private userId: string | null;
+    private token: string | null;
 
     constructor(serverUrl: string) {
-        try {
-            this.token = localStorage.getItem('userToken');
+        this.token = localStorage.getItem('userToken');
 
-            if (!this.token) {
-                throw new Error('Token not found. Please log in.');
-            }
-
-            const decodedUser = jwtDecode<DecodedToken>(this.token);
-
-            if (!decodedUser || !decodedUser._id) {
-                throw new Error('Invalid token. Please log in.');
-            }
-
-            this.userId = decodedUser._id;
-
-            this.socket = io(serverUrl, {
-                query: {
-                    userId: this.userId,
-                    token: this.token,
-                },
-            });
-
-            this.setupListeners();
-            this.emitLogin(this.userId, this.token); // Automatically emit login event upon construction
-        } catch (error) {
-            // Redirect to login page or handle error appropriately
-            window.location.href = '/login';
+        if (!this.token) {
+            throw new Error('Token not found. Please log in.');
         }
-    }
 
-    public getSocket(): Socket | null {
-        return this.socket;
+        // Decode token to get userId
+        const decodedUser = jwtDecode<DecodedToken>(this.token);
+
+        if (!decodedUser) {
+            throw new Error('Invalid token. Please log in.');
+        }
+        this.userId = decodedUser._id;
+
+        if (!this.userId) {
+            throw new Error('User ID not found in token. Please log in.');
+        }
+
+        this.socket = io(serverUrl, {
+            query: {
+                userId: this.userId,
+                token: this.token,
+            },
+        });
+
+        this.setupListeners();
+        this.emitLogin(this.userId, this.token); // Automatically emit login event upon construction
     }
 
     private setupListeners(): void {
-        if (!this.socket) return;
-
         this.socket.on('connect', () => {
-            if (debug) console.log('Connected to server');
+            if(debug) console.log('connected to server');
         });
 
         this.socket.on('disconnect', () => {
-            if (debug) console.log('Disconnected from server');
+            if(debug) console.log('disconnected from server');
         });
 
+        // Handle custom events here
         this.socket.on('notification', (data) => {
-            if (debug) console.log('Notification received:', data);
+            if(debug) console.log('notification received:', data);
         });
 
+        // Handle authorization event from backend
         this.socket.on('authorized', () => {
-            if (debug) console.log('Authorization successful');
+            if(debug) console.log('Authorization successful');
+            // Handle any actions upon successful authorization, if needed
         });
 
         this.socket.on('unauthorized', (message: string) => {
-            if (debug) console.log('Authorization failed:', message);
+            if(debug) console.log('Authorization failed:', message);
+            // Handle unauthorized event, e.g., redirect to login page
         });
 
-        this.socket.on('followed_f', this.handleFollowedEvent.bind(this));
-        this.socket.on('liked_f', this.handleLikedEvent.bind(this));
-        this.socket.on('commented_f', this.handleCommentedEvent.bind(this));
-    }
+        this.socket.on('followed_f', (data: { sender: string; reciever: string; followStatus: string, notifyDetails: {
+            follow: boolean,
+            followed: boolean,
+            sender: {
+                id: string,
+                username: string,
+            },
+            reciever: {
+                id: string,
+                username: string,
+            }
+        }}) => {
+            if(debug) console.log('followed_f event received:', data);
+            if (data.reciever === this.userId) {
+                if (data.followStatus === 'followed' && data.notifyDetails.follow) {
+                    notifyInfo(`${data.notifyDetails.sender.username} followed you back`);
+                } else {
+                    notifyInfo(`${data.notifyDetails.sender.username} ${(data.followStatus === 'followed' ? 'started' : 'stopped')} following you`);
+                }
+            }
+            // Handle follow event here
+        });
 
-    private handleFollowedEvent(data: { sender: string; reciever: string; followStatus: string; notifyDetails: {
-        follow: boolean;
-        followed: boolean;
-        sender: {
-            id: string;
-            username: string;
-        };
-        reciever: {
-            id: string;
-            username: string;
-        };
-    }}) {
-        if (debug) console.log('Followed event received:', data);
+        this.socket.on('liked_f', (data: { sender: {
+            id: string,
+            username: string,
+        }; post: string }) => {
+            if(debug) console.log('liked_f event received:', data);
+            if(data.sender.id !== this.userId) notifyInfo(`${data.sender.username} liked your post`);
+        });
 
-        if (data.reciever === this.userId) {
-            const { sender, followStatus, notifyDetails } = data;
-            const followMessage = notifyDetails.follow ? 'followed you back' : 
-                (followStatus === 'followed' ? 'started following you' : 'stopped following you');
-            notifyInfo(`${sender.username} ${followMessage}`);
-        }
-    }
+        this.socket.on('commented_f', (data: { sender: {
+            id: string,
+            username: string
+        } }) => {
+            if(debug) console.log('commented_f event received:', data);
+            if(data.sender.id !== this.userId) notifyInfo(`${data.sender.username} commented on your post`);
+        });
 
-    private handleLikedEvent(data: { sender: { id: string; username: string }; post: string }) {
-        if (debug) console.log('Liked event received:', data);
-
-        if (data.sender.id !== this.userId) {
-            notifyInfo(`${data.sender.username} liked your post`);
-        }
-    }
-
-    private handleCommentedEvent(data: { sender: { id: string; username: string } }) {
-        if (debug) console.log('Commented event received:', data);
-
-        if (data.sender.id !== this.userId) {
-            notifyInfo(`${data.sender.username} commented on your post`);
-        }
+        // Add more event listeners as needed
     }
 
     private emitLogin(userId: string, token: string): void {
-        if (!this.socket) return;
         this.socket.emit('login', { userId, token });
-        if (debug) console.log('Login emitted with userId and token:', userId, token);
+        if(debug) console.log('Login emitted with userId and token:', userId, token);
     }
 
     public emit(event: string, data: any): void {
-        if (!this.socket) return;
         this.socket.emit(event, data);
     }
 
     public follow(followId: string): void {
-        if (!this.socket || !this.userId) return;
-        this.socket.emit('follow_b', { userId: this.userId, followId });
+        if (this.userId) {
+            this.socket.emit('follow_b', { userId: this.userId, followId });
+        }
     }
 
     public on(event: string, callback: (data: any) => void): void {
-        if (!this.socket) return;
         this.socket.on(event, callback);
     }
 
     public off(event: string, callback?: (data: any) => void): void {
-        if (!this.socket) return;
         this.socket.off(event, callback);
     }
 }
