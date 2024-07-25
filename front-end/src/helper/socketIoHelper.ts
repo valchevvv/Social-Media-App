26b7bf65977545993a093609419e6054795dc4b1
Daniel Valchev <dvalchevvv@gmail.com>
2024-07-25 12:09:09 +0300

    Small fixes

--- front-end/src/helper/socketIoHelper.ts
+++ front-end/src/helper/socketIoHelper.ts
@@ -25,7 +25,6 @@ export class SocketIoHelper {
         }
 
         this.userId = decodedUser._id;
-        console.log('userId:', this.userId);
 
         this.socket = io(serverUrl, {
             extraHeaders: {
