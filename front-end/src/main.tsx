5ce8847ed668da33d7d9a7198e6ad8e1301c23ef
Daniel Valchev <dvalchevvv@gmail.com>
2024-08-01 14:31:57 +0300

    Offline page PWA

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -21,6 +21,16 @@ const updateSW = registerSW({
   },
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, (error) => {
+          console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
--- front-end/src/service-worker.js
+++ front-end/src/service-worker.js
@@ -1,12 +1,30 @@
 // src/service-worker.js
-self.addEventListener('install', (event) => {
+
+const CACHE_NAME = 'my-pwa-cache-v1';
+const OFFLINE_URL = 'offline.html';
+
+self.addEventListener('install', () => {
     console.log('Service Worker installing.');
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', () => {
     console.log('Service Worker activating.');
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request).catch(() => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    return cache.match(OFFLINE_URL);
+                });
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
